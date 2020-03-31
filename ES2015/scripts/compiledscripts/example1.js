79483ba0a2ad30f7028c8748c10dccde7d4ed915
harishtm <harishtm77@gmail.com>
2020-03-31 20:01:15 +0530

    Changed configuration to move all compiled files to a different folder

--- ES2015/scripts/compiledscripts/example1.js
+++ ES2015/scripts/compiledscripts/example1.js
@@ -1,18 +1,17 @@
 "use strict";
 /*
-
+1. Hoisting
+2. Functional scope
+*/
+/*
 Hoisting is a JavaScript mechanism where variables and function declarations are moved
 to the top of their scope before code execution.
 Inevitably, this means that no matter where functions and variables are declared,
 they are moved to the top of their scope regardless of whether their scope is global or local.
-
 */
-/*
-
-"var" declaration is function scope
-
-*/
+/* "var" declaration is function scope */
 function greetMessage(name) {
+    /* var declaration has functional scope the value will be accessible within the function*/
     if (name == "John") {
         var msg = "Hello " + name;
     }
@@ -20,5 +19,9 @@ function greetMessage(name) {
         var msg = "Hello there";
     }
     console.log(msg);
+    // var msg; 
+    /* this is called hositing as the variable
+       can also be declared at the bottom
+       (any var declaration is going to be moved to the very top of the function) */
 }
 greetMessage("John");
--- ES2015/scripts/example1.ts
+++ ES2015/scripts/example1.ts
@@ -1,25 +1,31 @@
 /*
+1. Hoisting
+2. Functional scope
+*/
 
+/*
 Hoisting is a JavaScript mechanism where variables and function declarations are moved 
 to the top of their scope before code execution.
 Inevitably, this means that no matter where functions and variables are declared,
 they are moved to the top of their scope regardless of whether their scope is global or local.
-
 */
 
-/*
-
-"var" declaration is function scope
-
-*/
+/* "var" declaration is function scope */
 
 function greetMessage(name: any){
+
+    /* var declaration has functional scope the value will be accessible within the function*/
+
     if(name == "John"){
         var msg = "Hello " + name;
     } else {
         var msg = "Hello there"
     }
     console.log(msg)
+    // var msg; 
+    /* this is called hositing as the variable 
+       can also be declared at the bottom
+       (any var declaration is going to be moved to the very top of the function) */
 }
 
 greetMessage("John")
