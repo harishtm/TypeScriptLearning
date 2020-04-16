b9ebdc7cfba3d7e0d277676951f7a44195a8889c
harishtm <harishtm77@gmail.com>
2020-04-16 08:36:29 +0530

    object and methos in es6

--- ES2015/scripts/compiledscripts/example17.js
+++ ES2015/scripts/compiledscripts/example17.js
@@ -1,9 +1,34 @@
 "use strict";
+/*
+
+Class body and method definition
+
+Note: In ES2015 and not TypeScript a class body can contain only methods and not properties
+
+*/
+/*
+
+1. Constructor method - Is used for creating and initializing an object and for every class
+                        there can only be one constructor.
+
+2. Static Method - Which can be called without instantiating a class
+                   It can not be called with object
+
+3. Prototype Method - Which can be called with each object
+*/
 var Employee = /** @class */ (function () {
     function Employee(name) {
         this.name = name;
         console.log(this.name + " Constructor");
     }
+    Employee.dearnessAllowance = function () {
+        console.log("Static Method");
+    };
+    Employee.prototype.greetEmployee = function () {
+        console.log("Hello " + this.name);
+    };
     return Employee;
 }());
 var emp = new Employee("John");
+Employee.dearnessAllowance();
+emp.greetEmployee();
--- ES2015/scripts/example17.ts
+++ ES2015/scripts/example17.ts
@@ -1,8 +1,37 @@
+/* 
+
+Class body and method definition 
+
+Note: In ES2015 and not TypeScript a class body can contain only methods and not properties
+
+*/
+
+
+/* 
+
+1. Constructor method - Is used for creating and initializing an object and for every class
+                        there can only be one constructor.
+
+2. Static Method - Which can be called without instantiating a class 
+                   It can not be called with object
+
+3. Prototype Method - Which can be called with each object
+*/
+
 class Employee{
     constructor(name:string){
         this.name = name;
         console.log(this.name + " Constructor")
     }
+
+    static dearnessAllowance(){
+        console.log("Static Method")
+    }
+    greetEmployee(){
+        console.log("Hello " + this.name)
+    }
 }
 
 let emp = new Employee("John");
+Employee.dearnessAllowance();
+emp.greetEmployee()
