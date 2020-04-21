9dfcefce93806a43cc5a908f1240bbb35dbcb14b
harishtm <harishtm77@gmail.com>
2020-04-21 10:41:32 +0530

    examples of set and usability

--- ES2015/scripts/compiledscripts/example19.js
+++ ES2015/scripts/compiledscripts/example19.js
@@ -18,3 +18,17 @@ console.log(myMap[obj1]);
 // toString representation of both objects obj1 and obj2 are same
 console.log(obj1.toString(), obj2.toString());
 console.log(obj1.toString() === obj2.toString());
+var myNewSet = new Set();
+var obj3 = {};
+var obj4 = {};
+myNewSet.add("Hello");
+myNewSet.add(1);
+myNewSet.add(1); // Will be not be added as 1 is repeated
+myNewSet.add(obj3); // since it is not converted to toString obj3 and obj4 are not identical
+myNewSet.add(obj4);
+console.log(myNewSet.size);
+var newSet = new Set([1, 2, 3, 4, 4, 4]);
+console.log("Size of new set ", newSet.size); // 4 as other values are repeated
+var chainSet = new Set().add("Hello").add("World");
+console.log("Size of chain set", chainSet.size);
+console.log(newSet.delete(1)); // deleting an element from set
