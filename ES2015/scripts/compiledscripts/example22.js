3c337e81ef56c4d6d2ed7400ec5a71e8cbd237a5
harishtm <harishtm77@gmail.com>
2020-06-12 20:09:13 +0530

    Symbols in ES6

---
+++ ES2015/scripts/compiledscripts/example22.js
@@ -0,0 +1,4 @@
+"use strict";
+// Symbols (new primitive type in ES2015/ES6)
+var s = Symbol();
+console.log(typeof (s));
---
+++ ES2015/scripts/example22.ts
@@ -0,0 +1,4 @@
+// Symbols (new primitive type in ES2015/ES6)
+
+let s = Symbol();
+console.log(typeof(s))
