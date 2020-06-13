8239075161b0441c915721076e494044f3da3e59
harishtm <harishtm77@gmail.com>
2020-06-13 19:06:45 +0530

    Symbol registry

--- ES2015/scripts/compiledscripts/example22.js
+++ ES2015/scripts/compiledscripts/example22.js
@@ -1,4 +1,31 @@
 "use strict";
 // Symbols (new primitive type in ES2015/ES6)
-var s = Symbol();
+var _a;
+// Symbol can not be used with new 
+var s = Symbol("First Symbol");
+// ES2015 has a new type "symbol"
 console.log(typeof (s));
+console.log(s.toString());
+var s2 = Symbol("Test");
+var s3 = Symbol("Test");
+// Symbol always create a unique id
+console.log(s2 === s3); // false
+/*
+    Symbol registry - it can be use the same Symbol again in a different or a same file
+    for that purpose we have a Symbol registry
+*/
+var s4 = Symbol.for("RegistrySymbol");
+var s5 = Symbol.for("RegistrySymbol");
+/*
+    Symbol.for does not add the symbol right away it checks for the Symbol with the key
+    "RegistrySymbol" already exist in the registry if it does it will return that symbol
+    s4. If it does not exist it will create in the Symbol registry
+*/
+console.log(s4 == s5);
+console.log(Symbol.keyFor(s4));
+var f_name = Symbol();
+var p_person = (_a = {},
+    _a[f_name] = "John",
+    _a);
+console.log(Object.getOwnPropertyNames(p_person));
+console.log(Object.getOwnPropertySymbols(p_person));
--- ES2015/scripts/example22.ts
+++ ES2015/scripts/example22.ts
@@ -1,4 +1,41 @@
 // Symbols (new primitive type in ES2015/ES6)
 
-let s = Symbol();
+
+// Symbol can not be used with new 
+
+let s = Symbol("First Symbol");
+
+// ES2015 has a new type "symbol"
+
 console.log(typeof(s))
+console.log(s.toString())
+
+let s2 = Symbol("Test");
+let s3 = Symbol("Test");
+
+// Symbol always create a unique id
+console.log(s2 === s3) // false
+
+/* 
+    Symbol registry - it can be use the same Symbol again in a different or a same file 
+    for that purpose we have a Symbol registry 
+*/
+
+let s4 = Symbol.for("RegistrySymbol");
+let s5 = Symbol.for("RegistrySymbol");
+
+/* 
+    Symbol.for does not add the symbol right away it checks for the Symbol with the key    
+    "RegistrySymbol" already exist in the registry if it does it will return that symbol
+    s4. If it does not exist it will create in the Symbol registry
+*/
+
+console.log(s4 == s5)
+console.log(Symbol.keyFor(s4));
+
+let f_name = Symbol();
+let p_person = {
+    [f_name]: "John"
+};
+console.log(Object.getOwnPropertyNames(p_person));
+console.log(Object.getOwnPropertySymbols(p_person));
