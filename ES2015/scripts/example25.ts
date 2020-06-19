bc5d0b86175e3a2c5ef4dc3c073c1265bd81426e
harishtm <harishtm77@gmail.com>
2020-06-19 20:10:36 +0530

    variable declaration

---
+++ ES2015/scripts/example25.ts
@@ -0,0 +1,15 @@
+// Different ways of declaration typescript variable 
+
+// Type inference
+
+var x = 10; // on hover of x shows number
+var y = "hello"; //  on hover of y shows string
+var z = true; // on hover gives z as boolean
+
+// Explicit type
+
+// For that we make use of annotations (:)
+
+var xx: number = 10;
+var yy: string = "Welcome to typescript"; // Initialization is not compulsory
+var zz:boolean;
